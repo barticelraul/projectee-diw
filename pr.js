fab2c4d2791fa505776e62d8725cececc7f4d19d
Raul Barticel <raba122@vidalibarraquer.net>
2026-04-07 09:37:03 +0200

    Actualización de index.html y archivos multimedia

--- pr.js
+++
@@ -1,6 +0,0 @@
-const productos = [
-    { id: 1, nombre: 'Kebab Mixto', precio: 6.50, categoria: 'kebab', img: 'https://images.unsplash.com/photo-1529006557810-274b9b2fc783?auto=format&fit=crop&w=300&q=80' },
-    { id: 2, nombre: 'Pizza Margarita', precio: 8.90, categoria: 'pizza', img: 'https://images.unsplash.com/photo-1513104890138-7c749659a591?auto=format&fit=crop&w=300&q=80' },
-    { id: 3, nombre: 'Taco Francés XL', precio: 9.50, categoria: 'tacos', img: 'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?auto=format&fit=crop&w=300&q=80' },
-    { id: 4, nombre: 'Taco Mexicano', precio: 3.50, categoria: 'tacos', img: 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=300&q=80' }
-];
